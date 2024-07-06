e037171e60452d31afb6c6ed7e73dbe61784294d
Manthan Patidar <manthanpatidar@gmail.com>
2024-07-06 22:19:52 +0530

    changes

--- src/main/java/com/manthan/rkesnotificationservice/consumers/NotificationConsumer.java
+++ src/main/java/com/manthan/rkesnotificationservice/consumers/NotificationConsumer.java
@@ -1,13 +1,16 @@
 package com.manthan.rkesnotificationservice.consumers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationConsumer {
+    private static final Logger LOG = LoggerFactory.getLogger(NotificationConsumer.class);
 
-    @KafkaListener(topics="sendNotification", groupId = "notificationService")
+    @KafkaListener(topics="sendNotification", groupId = "rkesNotificationService")
     public void handleNotification(String message) {
-        System.out.println(message);
+        LOG.info("topic:sendNotification -- message:"+message);
     }
 }
